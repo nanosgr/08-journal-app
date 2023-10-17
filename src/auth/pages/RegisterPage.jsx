f57468cec364d2e2af5079541801ec989864c9a3
nanosgr <sebastian.g.rios@gmail.com>
2023-10-17 17:15:35 -0300

    Actualizamos app

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,5 +1,45 @@
+import { Link as RouterLink } from 'react-router-dom';
+import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { AuthLayout } from '../layout/AuthLayout';
+
 export const RegisterPage = () => {
     return (
-        <div>RegisterPage</div>
+        <AuthLayout title="Crear Cuenta">
+            <form>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField 
+                            label="Nombre Completo"
+                            type='text' 
+                            placeholder='Pepe Hongo'
+                            fullWidth />
+                    </Grid>
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField 
+                            label="Correo"
+                            type='email' 
+                            placeholder='correo@email.com'
+                            fullWidth />
+                    </Grid>
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField 
+                            label="Contraseña"
+                            type='password' 
+                            fullWidth />
+                    </Grid>
+                </Grid>
+                <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+                    <Grid item xs={12}>
+                        <Button variant="contained" fullWidth>Crear Cuenta</Button>
+                    </Grid>
+                </Grid>
+                <Grid container direction='row' justifyContent='end'>
+                    <Typography sx={{ mr: 1 }}>¿Ya tienes una cuenta?</Typography>
+                    <Link component={ RouterLink } color='inherit' to='/auth/login'>
+                        Ingresar
+                    </Link>
+                </Grid>
+            </form>
+        </AuthLayout>
     )
 }
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,7 +1,28 @@
-import { Typography } from "@mui/material"
+// Note: Journal Page Component
+import { IconButton } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
+import { NoteView, NothingSelectedView } from "../views"
+import { AddOutlined } from "@mui/icons-material"
 
 export const JournalPage = () => {
     return (
-        <Typography variant="h1">Journal Page</Typography>
+        <JournalLayout>
+            {/* <NothingSelectedView /> */}
+            <NoteView />
+
+            <IconButton
+                size="large"
+                sx={{
+                    color: 'white',
+                    backgroundColor: 'error.main',
+                    ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+                    position: 'fixed',
+                    right: 50,
+                    bottom: 50,
+                }}
+            >
+                <AddOutlined sx={{ fontSize: 30 }}/>
+            </IconButton>
+        </JournalLayout>
     )
 }
